b1174721a6df68ae77f70f4ece95abe03e113a86
cuongg-github <qc01696@gmail.com>
2024-11-02 19:28:11 +0700

    add model

---
+++ BusinessObjects/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class Student
+{
+    public string Idstudent { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public DateOnly? BirthDay { get; set; }
+
+    public string? PassWord { get; set; }
+
+    public string? IsActive { get; set; }
+}
