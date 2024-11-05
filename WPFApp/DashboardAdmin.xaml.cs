bdb39c9a37ec46704819c3a75ea08c90a0fdd6f0
Bao1404 <lethebao1404@gmail.com>
2024-11-05 13:32:08 +0700

    dashboard admin, teacher, user

---
+++ WPFApp/DashboardAdmin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for DashboardAdmin.xaml
+    /// </summary>
+    public partial class DashboardAdmin : Window
+    {
+        public DashboardAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Info(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_AddUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_ManageUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPFApp/DashboardTeacher.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for DashboardTeacher.xaml
+    /// </summary>
+    public partial class DashboardTeacher : Window
+    {
+        public DashboardTeacher()
+        {
+            InitializeComponent();
+        }
+
+        private void logout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            LoginWindow logn = new LoginWindow();
+            logn.Show();
+        }
+
+        private void Btn_Info(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Search(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_UpdateScore(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_MngClass(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_MngStudent(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Report(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Regulation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
