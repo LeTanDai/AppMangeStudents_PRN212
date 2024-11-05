e52e6b23bfa751ebed9e5e524614d1820f9a2bbb
Nguyen Van Hoang Phuc <hoangphucnguyenvan1@gmail.com>
2024-11-05 22:49:14 +0700

    Update

---
+++ BusinessObjects/Assign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class Assign
+{
+    public string Idteacher { get; set; } = null!;
+
+    public string Idsubject { get; set; } = null!;
+
+    public string NameClass { get; set; } = null!;
+
+    public string SchoolYear { get; set; } = null!;
+}
---
+++ BusinessObjects/StudentClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class StudentClass
+{
+    public string Idstudent { get; set; } = null!;
+
+    public string NameClass { get; set; } = null!;
+
+    public string SchoolYear { get; set; } = null!;
+}
