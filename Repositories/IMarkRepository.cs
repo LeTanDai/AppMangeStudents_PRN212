c95ce94ce83dcf2f75dc9a19ae844eaf33a52fb4
jronewo <trongboigaren2107@gmail.com>
2024-11-14 16:16:14 +0700

    Add UPdate score

---
+++ Repositories/IMarkRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObjects;
+using BusinessObjects.Models;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public interface IMarkRepository
+    {
+        void Updatemark(Mark mark);
+        IEnumerable<Mark> GetallMarks();
+        IEnumerable<Mark> GetMarksByClass(string className);
+        IEnumerable<Mark> GetMarksBySubject(string subjectId);
+        IEnumerable<Mark> GetMarksBySemester(string semester);
+        string GetSchoolYearById(string idStudent);
+        string GetSemById(string idStudent);
+    }
+}
---
+++ Repositories/MarkRepository.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObjects;
+using BusinessObjects.Models;
+using DAL;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class MarkRepository : IMarkRepository
+    {
+        
+        public void Updatemark(Mark mark) => MarkDAO.Instance.Updatemark(mark);
+
+     
+        public IEnumerable<Mark> GetallMarks() => MarkDAO.Instance.GetallMarks();
+
+
+        public IEnumerable<Mark> GetMarksByClass(string className) => MarkDAO.Instance.GetMarksByClass(className);
+
+
+        public IEnumerable<Mark> GetMarksBySubject(string subjectId) => MarkDAO.Instance.GetMarksBySubject(subjectId);
+
+
+        public IEnumerable<Mark> GetMarksBySemester(string semester) => MarkDAO.Instance.GetMarksBySemester(semester);
+        public string GetSchoolYearById(string idStudent) => MarkDAO.Instance.GetSchoolYearById(idStudent);
+        public string GetSemById(string idStudent) => MarkDAO.Instance.GetSemById(idStudent);
+    }
+}
